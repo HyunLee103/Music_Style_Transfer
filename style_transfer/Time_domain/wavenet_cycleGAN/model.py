fd38d474db0665296e578741606b9a30eed53277
2016047074 <dell0616@gmail.com>
2020-07-10 18:18:10 +0900

    time_dataload

--- style_transfer/Time_domain/wavenet_cycleGAN/model.py
+++ style_transfer/Time_domain/wavenet_cycleGAN/model.py
@@ -2,6 +2,9 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import librosa
+from torch.utils.data import Dataset, DataLoader
+import numpy as np
+from tqdm import tqdm
 
 """
 # mu-law function in 'music-translation' git
@@ -25,6 +28,7 @@ def inv_mu_law(x, mu=255.0):
 remove condition func. We don't use condition
 """
 
+
 class CausalConv1d(nn.Conv1d):
     def __init__(self,
                  in_channels,
@@ -46,28 +50,28 @@ class CausalConv1d(nn.Conv1d):
 
 
 class WavenetLayer(nn.Module):
-    def __init__(self, residual_channels, skip_channels, cond_channels,
+    def __init__(self, residual_channels, skip_channels,
                  kernel_size=2, dilation=1):
         super(WavenetLayer, self).__init__()
 
         self.causal = CausalConv1d(residual_channels, 2 * residual_channels,
                                    kernel_size, dilation=dilation, bias=True)
-        self.condition = nn.Conv1d(cond_channels, 2 * residual_channels,
-                                   kernel_size=1, bias=True)
+        # self.condition = nn.Conv1d(cond_channels, 2 * residual_channels,
+        #                            kernel_size=1, bias=True)
         self.residual = nn.Conv1d(residual_channels, residual_channels,
                                   kernel_size=1, bias=True)
         self.skip = nn.Conv1d(residual_channels, skip_channels,
                               kernel_size=1, bias=True)
 
-    def _condition(self, x, c, f):
-        c = f(c)
-        x = x + c
-        return x
+    # def _condition(self, x, c, f):
+    #     c = f(c)
+    #     x = x + c
+    #     return x
 
-    def forward(self, x, c=None):
+    def forward(self, x):
         x = self.causal(x)
-        if c is not None:
-            x = self._condition(x, c, self.condition)
+        # if c is not None:
+        #     x = self._condition(x, c, self.condition)
 
         assert x.size(1) % 2 == 0
         gate, output = x.chunk(2, 1)
@@ -82,85 +86,89 @@ class WavenetLayer(nn.Module):
 
 
 class WaveNet(nn.Module):
-    def __init__(self, args, create_layers=True, shift_input=True):
+    def __init__(self, class_num, shift_input=True, generator=True):
         super().__init__()
 
-        self.blocks = args.blocks
-        self.layer_num = args.layers
-        self.kernel_size = args.kernel_size
-        self.skip_channels = args.skip_channels
-        self.residual_channels = args.residual_channels
-        self.cond_channels = args.latent_d
-        self.classes = 256
+        self.blocks = 4
+        self.layer_num = 10
+        self.kernel_size = 2
+        self.skip_channels = 128
+        self.residual_channels = 128
+        # self.cond_channels = args.latent_d
+        self.classes = class_num
         self.shift_input = shift_input
 
-        if create_layers:
-            layers = []
-            for _ in range(self.blocks):
-                for i in range(self.layer_num):
-                    dilation = 2 ** i
-                    layers.append(WavenetLayer(self.residual_channels, self.skip_channels, self.cond_channels,
-                                               self.kernel_size, dilation))
-            self.layers = nn.ModuleList(layers)
+        layers = []
+        for _ in range(self.blocks):
+            for i in range(self.layer_num):
+                dilation = 2 ** i
+                layers.append(WavenetLayer(self.residual_channels, self.skip_channels,
+                                            self.kernel_size, dilation))
+        self.layers = nn.ModuleList(layers)
 
         self.first_conv = CausalConv1d(1, self.residual_channels, kernel_size=self.kernel_size)
         self.skip_conv = nn.Conv1d(self.residual_channels, self.skip_channels, kernel_size=1)
-        self.condition = nn.Conv1d(self.cond_channels, self.skip_channels, kernel_size=1)
+        # self.condition = nn.Conv1d(self.cond_channels, self.skip_channels, kernel_size=1)
         self.fc = nn.Conv1d(self.skip_channels, self.skip_channels, kernel_size=1)
         self.logits = nn.Conv1d(self.skip_channels, self.classes, kernel_size=1)
 
-    def _condition(self, x, c, f):
-        c = f(c)
-        x = x + c
-        return x
-
     @staticmethod
-    def _upsample_cond(x, c):
-        bsz, channels, length = x.size()
-        cond_bsz, cond_channels, cond_length = c.size()
-        assert bsz == cond_bsz
-
-        if c.size(2) != 1:
-            c = c.unsqueeze(3).repeat(1, 1, 1, length // cond_length)
-            c = c.view(bsz, cond_channels, length)
-
-        return c
-
-    @staticmethod
-    def shift_right(x):  # hmm... help me hyun...
+    def shift_right(x):
         x = F.pad(x, (1, 0))
         return x[:, :, :-1]
 
-    def forward(self, x, c=None):
+    def forward(self, x):
         if x.dim() < 3:
             x = x.unsqueeze(1)
         if (not 'Half' in x.type()) and (not 'Float' in x.type()):
             x = x.float()
 
-        x = x / 255 - 0.5
+        # x = x / 255 - 0.5
 
         if self.shift_input:
             x = self.shift_right(x)
 
-        if c is not None:
-            c = self._upsample_cond(x, c)
+        # if c is not None:
+        #     c = self._upsample_cond(x, c)
 
         residual = self.first_conv(x)
         skip = self.skip_conv(residual)
 
         for layer in self.layers:
-            r, s = layer(residual, c)
+            r, s = layer(residual)
             residual = residual + r
             skip = skip + s
 
         skip = F.relu(skip)
         skip = self.fc(skip)
-        if c is not None:
-            skip = self._condition(skip, c, self.condition)
+        # if c is not None:
+        #     skip = self._condition(skip, c, self.condition)
         skip = F.relu(skip)
         skip = self.logits(skip)
+        
+        if not generator:
+            pass
+            # GlobalAveragePool
 
         return skip
+
+    # def _condition(self, x, c, f):
+    #     c = f(c)
+    #     x = x + c
+    #     return x
+
+    # @staticmethod
+    # def _upsample_cond(x, c):
+    #     bsz, channels, length = x.size()
+    #     cond_bsz, cond_channels, cond_length = c.size()
+    #     assert bsz == cond_bsz
+
+    #     if c.size(2) != 1:
+    #         c = c.unsqueeze(3).repeat(1, 1, 1, length // cond_length)
+    #         c = c.view(bsz, cond_channels, length)
+
+    #     return c
+
 
 """
 --ZDiscriminator--
@@ -188,3 +196,20 @@ class ZDiscriminator(nn.Module):
 
         mean = logits.mean(2)
         return mean
+    
+
+class WaveData(Dataset):
+    def __init__(self, jazz_path, rock_path):
+        super().__init__()
+        self.jazz = np.load(jazz_path)
+        self.rock = np.load(rock_path)
+        
+    def __len__(self):
+        return min(len(self.jazz), len(self.rock))
+    
+    def __getitem__(self, index):
+        return {'jazz': self.jazz[index], 'rock': self.rock[index]}
+    
+data = WaveData('/content/drive/My Drive/jazz_other_both.npy', '/content/drive/My Drive/rock_other_both.npy')
+
+loader = DataLoader(data, batch_size=64, shuffle=True)
