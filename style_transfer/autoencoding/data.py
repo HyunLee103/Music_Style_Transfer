47151ef14448668b7535ba2cf60911299552ee12
2016047074 <dell0616@gmail.com>
2020-07-29 23:46:13 +0900

    wave_ae

--- style_transfer/autoencoding/data.py
+++ style_transfer/autoencoding/data.py
@@ -7,16 +7,18 @@ class Dataset(Dataset):
     def __init__(self, A, B):
         self.A = A
         self.B = B
+        self.A_mu = np.array([mu_law(x) for x in self.A])
+        self.B_mu = np.array([mu_law(x) for x in self.B])
         
     def __len__(self):
         return min(len(self.A), len(self.B))
     
     def __getitem__(self, index):
         
-        return {'A': mu_law(self.A[index]),
-                'B': mu_law(self.B[index]),
-                'A_aug': mu_law(wave_augmentation(np.asfortranarray(self.A[index]))),
-                'B_aug': mu_law(wave_augmentation(np.asfortranarray(self.B[index])))}
+        return {'A': self.A_mu[index],
+                'B': self.B_mu[index],
+                'A_aug': mu_law(wave_augmentation(self.A[index])),
+                'B_aug': mu_law(wave_augmentation(self.B[index]))}
 
 class WaveData:
     def __init__(self, A_path, B_path):
@@ -30,3 +32,43 @@ class WaveData:
         return DataLoader(Dataset(self.A, self.B),
                           batch_size=batch_size,
                           shuffle=shuffle)
+
+
+###############################3
+# for multiple domain
+class WaveData:
+    def __init__(self, paths):
+        self.data = [np.load(path) for path in paths]
+        
+        for path, data in zip(paths, self.data):
+            print(f"{path} length: {len(data)}")
+        
+    def get_loader(self, batch_size, shuffle=True):
+        return DataLoader(Dataset(self.data),
+                          batch_size=batch_size,
+                          shuffle=shuffle)
+        
+class Dataset(Dataset):
+    def __init__(self, data):
+        self.data = data
+        self.length = max([len(d) for d in self.data])
+        self.data_mu = [np.array(list(map(mu_law, data))) for data in self.data]
+        
+    def __len__(self):
+        return self.length
+    
+    def __getitem__(self, index):
+        out = dict()
+        out_aug = dict()
+        
+        for i, (data, data_mu) in enumerate(zip(self.data, self.data_mu)):
+            try:
+                out[i] = data_mu[index]
+                out_aug[i] = mu_law(wave_augmentation(data[index]))
+                
+            except IndexError:
+                idx = index - len(data)
+                out[i] = data_mu[idx]
+                out_aug[i] = mu_law(wave_augmentation(data[idx]))
+        
+        return out, out_aug
--- style_transfer/autoencoding/train.py
+++ style_transfer/autoencoding/train.py
@@ -1,10 +1,39 @@
 from audio_encoding import WaveNet, Encoder, ZDiscriminator
 from data import WaveData
 import torch
-import torch.nn as nn
 import torch.nn.functional as F
 from itertools import chain
 
+# origin
+args = {
+    # encoder
+    "encoder_blocks": 3,
+    "encoder_layers": 10,
+    "encoder_channels": 128,
+    "latent_d": 64,
+    "encoder_func": 'relu',
+    
+    # decoder
+    "blocks": 4,
+    "layers": 14,
+    "kernel_size": 2, 
+    "skip_channels": 128,
+    "residual_channels": 128,
+    
+    # discriminator
+    "n_datasets": 4,
+    "d_layers": 3,
+    "d_channels": 100,
+    "d_lambda": 0.01,
+    "dropout_rate": 0.0,
+    
+    # train
+    "n_epochs": 15,
+    "batch_size": 8,
+    "lr": 0.001
+}
+
+# custom for batch 8
 args = {
     # encoder
     "encoder_blocks": 2,
@@ -36,118 +65,85 @@ args = {
 # data load
 data_path = r"C:/Users/koo/Desktop/"
 
-wavedata = WaveData(A=data_path + "piano_cover.npy",
-                    B=data_path + "musdb_sample.npy")
+wavedata = WaveData([data_path + "piano.npy",
+                     data_path + "musdb_other.npy",
+                     data_path + "musdb_vocal.npy",
+                     data_path + "musdb_bass.npy"])
 
 dataloader = wavedata.get_loader(args["batch_size"],
                                  shuffle=True)
 
 # models
 encoder = Encoder(args).cuda()
-decoder_A = WaveNet(args).cuda()
-decoder_B = WaveNet(args).cuda()
+decoders = [WaveNet(args).cuda() for _ in range(args['n_datasets'])]
 z_discr = ZDiscriminator(args).cuda()
 
-# loss & optim
-#criterion = nn.CrossEntropyLoss().cuda()
-
-encoder_optim = torch.optim.Adam(encoder.parameters(), lr=args["lr"])
+# optim
 z_discr_optim = torch.optim.Adam(z_discr.parameters(), lr=args["lr"])
-A_optim = torch.optim.Adam(decoder_A.parameters(), lr=args["lr"])
-B_optim = torch.optim.Adam(decoder_B.parameters(), lr=args["lr"])
+optims = [torch.optim.Adam(chain(encoder.parameters(), decoder.parameters()), lr=args['lr']) \
+          for decoder in decoders]
+
+# pth
+bestmodel = torch.load(data_path + "bestmodel_1.pth")
+encoder.load_state_dict(bestmodel['encoder_state'])
+decoders[0].load_state_dict(bestmodel['decoder_state'])
+
+def train_discr(encoder, z_discr, z_discr_optim, x_aug, datanum):
+    ### train z_discr to A ###
+    z_discr_optim.zero_grad()
+    # forward
+    cond = encoder(x_aug.float())
+    discr = z_discr(cond.detach())
+    # get loss
+    loss = F.cross_entropy(discr, torch.tensor([datanum]*x_aug.size(0)).long().cuda())
+    loss_discr = loss * args['d_lambda']
+    # backward & step
+    loss_discr.backward()
+    z_discr_optim.step()
+    
+    return loss_discr.item()
+    
+def train_decoder(encoder, z_discr, decoder, decoder_optim,
+                  x, x_aug, datanum):
+    ### train decoder A ###
+    decoder_optim.zero_grad()
+    # forward
+    cond = encoder(x_aug.float())
+    discr = z_discr(cond)
+    out = decoder(x.float(), cond)  # (batch, 256, seq)
+    # manipulate for cross entropy
+    out = out.transpose(1, 2).contiguous().view(-1, 256)  # (batch * seq, 256)
+    target = x.view(-1).long()  # (batch * seq)
+    # get loss A
+    loss_discr = F.cross_entropy(discr, torch.tensor([datanum]*x.size(0)).long().cuda())
+    loss_recon = F.cross_entropy(out, target, reduction='none')  # (batch * seq)
+    loss_recon = loss_recon.reshape(x.size(0), x.size(1)).mean(1).mean()  # (batch, seq) -> (batch) -> scalar
+    loss_decoder = loss_recon - loss_discr * args['d_lambda']
+    # backward & step
+    loss_decoder.backward()
+    decoder_optim.step()
+    
+    return loss_decoder.item()
 
 # train
 for epoch in range(1, args["n_epochs"]+1):
-    print(f"Epoch {epoch}")
-    for i, batch in enumerate(dataloader):
-        A = batch['A'].cuda()
-        B = batch['B'].cuda()
-        A_aug = batch['A_aug'].cuda()
-        B_aug = batch['B_aug'].cuda()
-        
-        # z_dicr target
-        zeros = torch.zeros(A.size(0)).long().cuda()
-        ones = torch.ones(A.size(0)).long().cuda()
- 
-        ### train z_discr to A ###
-        encoder.zero_grad()
-        z_discr.zero_grad()
-        
-        cond_A = encoder(A.float())
-        discr_A = z_discr(cond_A)
-        
-        # get loss
-        loss_A = F.cross_entropy(discr_A, zeros)
-        loss_discr = loss_A * args['d_lambda']
-        # backward
-        loss_discr.backward()
-        encoder_optim.step()
-        z_discr_optim.step()
-        
-        ### train decoder A ###
-        encoder.zero_grad()
-        decoder_A.zero_grad()
-        
-        cond_A = encoder(A_aug.float())
-        discr_A = z_discr(cond_A)
-        out_A = decoder_A(A.float(), cond_A)  # (batch, 256, seq)
-        
-        out_A = out_A.transpose(1, 2).contiguous().view(-1, 256)  # (batch * seq, 256)
-        target_A = A.view(-1).long()  # (batch * seq)
-        # get loss A
-        loss_discr = F.cross_entropy(discr_A, zeros)
-        loss_decoder = F.cross_entropy(out_A, target_A, reduction='none')  # (batch * seq)
-        loss_decoder = loss_decoder.reshape(A.size(0), A.size(1)).mean(1).mean()  # (batch, seq) -> (batch) -> scalar
-        loss_A_decoder = loss_decoder - loss_discr * args['d_lambda']
-        # backward & step
-        loss_A_decoder.backward()
-        encoder_optim.step()
-        A_optim.step()
-        
-        ### train z_discr to B ###
-        encoder.zero_grad()
-        z_discr.zero_grad()   
-        
-        cond_B = encoder(B.float())
-        discr_B = z_discr(cond_B)
-        # get loss
-        loss_B = F.cross_entropy(discr_B, ones)
-        loss_discr = loss_B * args['d_lambda']
-        # backward
-        loss_discr.backward()
-        encoder_optim.step()
-        z_discr_optim.step()
-        
-        ### train decoder B ###
-        encoder.zero_grad()
-        decoder_B.zero_grad()
-        
-        cond_B = encoder(B_aug.float())
-        discr_B = z_discr(cond_B)
-        out_B = decoder_B(B.float(), cond_B)  # (batch, 256, seq)
-        
-        out_B = out_B.transpose(1, 2).contiguous().view(-1, 256)  # (batch * seq, 256)
-        target_B = B.view(-1).long()  # (batch * seq)
-        # get loss B
-        loss_discr = F.cross_entropy(discr_B, ones)
-        loss_decoder = F.cross_entropy(out_B, target_B, reduction='none')  # (batch * seq)
-        loss_decoder = loss_decoder.reshape(B.size(0), B.size(1)).mean(1).mean()  # (batch, seq) -> (batch) -> scalar
-        loss_B_decoder = loss_decoder - loss_discr * args['d_lambda']
-        # backward & step
-        loss_B_decoder.backward()
-        encoder_optim.step()
-        B_optim.step()
-        
-        print(f"Batch {i+1}/{len(dataloader)}")
-        print(f"- [Discriminator] {loss_discr.item():0.4} / [A] {loss_A_decoder.item():0.4} / [B] {loss_B_decoder.item():0.4}")
-        
+    print(f"\nEpoch {epoch}")
+    for i, (x, x_aug) in enumerate(dataloader):
+        print(f"\nBatch {i+1}/{len(dataloader)}")
+        for datanum in range(args['n_datasets']):
+            d_loss = train_discr(encoder, z_discr, z_discr_optim,
+                        x_aug[datanum].cuda(), datanum)
+            
+            loss = train_decoder(encoder, z_discr, decoders[datanum], optims[datanum],
+                          x[datanum].cuda(), x_aug[datanum].cuda(), datanum)
+            
+            print(f"[{datanum}] {d_loss:0.4}, {loss:0.4}", end='  ')
+    
+    
     torch.save({
         'encoder': encoder.state_dict(),
-        'decoder_A': decoder_A.state_dict(),
-        'decoder_B': decoder_B.state_dict(),
+        'decoders': [decoder.state_dict() for decoder in decoders],
         'z_discr': z_discr.state_dict(),
-        'encoder_optim': encoder_optim.state_dict(),
-        'decoder_A_optim': A_optim.state_dict(),
-        'decoder_B_optim': B_optim.state_dict(),
-        'z_discr_optim': z_discr_optim.state_dict(),
+        'decoder_optims': [optim.state_dict() for optim in optims],
+        'z_discr_optim': z_discr_optim.state_dict()
     }, data_path + f"{epoch}.pth")
